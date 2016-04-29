bea94861b69a48a8874448ba68d4c6ff35184a5d
Ross Mackay <ross@mcky.me>
2016-04-29 15:46:09 +0100

    Refactor `getPostByFilename` and break out file-path handling

--- src/jekyll-adapters/posts.js
+++ src/jekyll-adapters/posts.js
@@ -1,6 +1,7 @@
 import fs from 'mz/fs'
 import path from 'path'
 import flatten from 'lodash/flatten'
+import last from 'lodash/last'
 import frontMatter from 'yaml-front-matter'
 
 // Temporary while rapidly testing
@@ -11,12 +12,26 @@ const hasAllowedExtension = (filename) => {
 	return path.extname(filename).substr(1) === 'md'
 }
 
-const getPostByFilename = (filename) => {
+
+const formatPostObject = (name, fullPath = '') => {
+	// Naievely assumes posts/drafts are only ever one level deep
+	const type = last(fullPath.split(path.sep)).slice(1, -1)
+	return {
+		path: path.join(fullPath, name),
+		type,
+		name,
+	}
+}
+
+
+const getPostByFilename = (filename, {getLocation} = {getLocation: true}) => {
 	return fs.readFile(filename, 'utf8')
 		.then(file => {
 			const {__content: content, ...info} = frontMatter.loadFront(file)
 			return {info, content}
 		})
+		.then(file => getLocation ? {...file, ...formatPostObject(filename)} : file)
+		.catch(console.log)
 }
 
 
@@ -26,28 +41,29 @@ const getPosts = () => {
 
 		return fs.readdir(fullPath)
 				.then(files => files.filter(hasAllowedExtension))
-				.then(files => files.map(name => ({
-					folder,
-					name,
-					path: path.join(fullPath, name),
-				})))
+				.then(files => files.map(name => formatPostObject(name, fullPath)))
+				.catch(console.log)
 	})
 
 	return Promise.all(folderPromises)
 			.then(flatten)
+			.catch(console.log)
 }
+
 
 const getExpandedPosts = () => {
 	return getPosts()
 		.then(posts => {
 			return Promise.all(posts.map(post => {
-				return getPostByFilename(post.path)
+				return getPostByFilename(post.path, {getLocation: false})
 					.then(fullPost => ({...post, ...fullPost}))
+					.catch(console.log)
 			}))
 	})
 }
 
 export {
+	formatPostObject,
 	getPostByFilename,
 	getPosts,
 	getExpandedPosts,
