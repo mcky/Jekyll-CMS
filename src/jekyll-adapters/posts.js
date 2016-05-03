71d6b48fce683c3342599dc68bb1a018d9c25647
Ross Mackay <ross@mcky.me>
2016-05-03 16:37:00 +0100

    Add more allowed extensions and add `formatting` key to posts

--- src/jekyll-adapters/posts.js
+++ src/jekyll-adapters/posts.js
@@ -7,18 +7,30 @@ import frontMatter from 'yaml-front-matter'
 // Temporary while rapidly testing
 const baseDir = process.env.JEKYLL_DIR
 
-const hasAllowedExtension = (filename) => {
-	// @TODO: Just markdown for now - replace with array of allowed
-	return path.extname(filename).substr(1) === 'md'
+const formats = {
+	'md': 'markdown',
+	'markdown': 'markdown',
+	'txt': 'plaintext',
 }
 
+const hasAllowedExtension = (filename) => {
+	const extension = path.extname(filename).substr(1)
+	return formats.hasOwnProperty(extension)
+}
+
+const getContentFormat = (extension) => {
+	return formats[extension.substr(1)] || null
+}
 
 const formatPostObject = (postPath) => {
 	// Naievely assumes posts/drafts are only ever one level deep
 	const type = nth(postPath.split(path.sep), -2).slice(1, -1)
+		, pathObj = path.parse(postPath)
+
 	return {
 		path: postPath,
-		filename: path.parse(postPath).base,
+		filename: pathObj.base,
+		formatting: getContentFormat(pathObj.ext),
 		type,
 	}
 }
