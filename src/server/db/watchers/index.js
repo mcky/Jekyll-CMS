26c7c258935e6b6486154129e3c179e94cf79548
Ross Mackay <ross@mcky.me>
2016-05-04 16:33:19 +0100

    Update watchers to include config file

--- src/server/db/watchers/index.js
+++ src/server/db/watchers/index.js
@@ -3,6 +3,7 @@ import chokidar from 'chokidar'
 
 import db from '../'
 import {getPostByFilename, formatPostObject} from '../../../jekyll-adapters/posts'
+import {getConfig} from '../../../jekyll-adapters/config'
 
 const dir = process.env.JEKYLL_DIR
 
@@ -22,14 +23,30 @@ const watchPosts = () => new Promise((resolve, reject) => {
 				.then(fileObj => db.posts.insert(fileObj))
 				.catch(console.log)
 		})
-		.on('unlink', path => {
-			db.posts.remove({path})
+		.on('unlink', path => db.posts.remove({path}))
+		.on('ready', resolve)
+})
+
+
+const watchConfig = () => new Promise((resolve, reject) => {
+
+	chokidar.watch(path.join(dir, '_config.yml'))
+		.on('change', path => {
+			getConfig(path)
+				.then(config => db.config.update({}, config))
+				.catch(console.log)
 		})
+		.on('add', path => {
+			getConfig(path)
+				.then(config => db.config.insert(config))
+				.catch(console.log)
+		})
+		.on('unlink', path => db.config.remove({}))
 		.on('ready', resolve)
 })
 
 const watchAll = () => new Promise((resolve, reject) => {
-	Promise.all([watchPosts()])
+	Promise.all([watchPosts(), watchConfig()])
 		.then(resolve)
 		.catch(reject)
 })
