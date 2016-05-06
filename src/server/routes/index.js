f1995ea4cba2653c23e2e6e480f8d89793552bc2
Ross Mackay <ross@mcky.me>
2016-05-06 17:56:24 +0100

    Serve index.html for all non api/public routes

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import { Router } from 'express'
 import {postController, configController} from '../controllers'
 import api from './api'
@@ -5,5 +6,7 @@ import api from './api'
 const router = Router()
 
 router.use('/api', api)
+
+router.get('/*', (req, res) => res.sendFile(path.join(__dirname, '../index.html')))
 
 export default router
