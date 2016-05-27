9c287204d65a18434089bb102d6155d9d75f7d2f
Ross Mackay <ross@mcky.me>
2016-05-27 16:07:15 +0100

    Update actionTypes to be more useful

--- src/ui/constants/actions.js
+++ src/ui/constants/actions.js
@@ -1,16 +1,21 @@
+import mapValues from 'lodash/mapValues'
 import keyMirror from '../utils/keymirror'
 
 const actionNames = [
 	'FETCH_POSTS',
 	'SET_POSTS',
+
+	'FETCH_POST',
+	'SET_POST',
 ]
 
 const actions = keyMirror(actionNames)
 
-const actionTypes = actionNames.map(a => ({type: a}))
+const actionTypes = mapValues(actions, (k, v) => ({type: v}))
 
 
 export default actions
 export {
+	actionNames,
 	actionTypes,
 }
