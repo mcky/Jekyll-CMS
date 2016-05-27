a43495661413bb8a4476be960fd74470e65f003a
Ross Mackay <ross@mcky.me>
2016-05-27 16:36:05 +0100

    Add `mergeState` reducer utils for common actions

--- src/ui/reducers/utils.js
+++ src/ui/reducers/utils.js
@@ -1,8 +1,26 @@
+import mapKeys from 'lodash/mapKeys'
+
+
 const reducerUtil = (reducers, state = {}, action) => {
 	const reducerFn = reducers[action.type]
 	return reducerFn ? (reducerFn(state, action)) : state
 }
 
+const mergeState = (state, action) => {
+	const {type, ...data} = action
+	return {
+		...state,
+		...data,
+	}
+}
+
+const mergeStateRenameKeys = (replacements, state, action) => {
+	const replaced = mapKeys(action, (v, k) => replacements[k] ? replacements[k] : k)
+	return mergeState(state, replaced)
+}
+
 export {
 	reducerUtil,
+	mergeState,
+	mergeStateRenameKeys,
 }
