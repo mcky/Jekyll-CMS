129522d6ed382ea30ab7a9d1384dff836c62bb28
Ross Mackay <ross@mcky.me>
2016-05-27 16:36:32 +0100

    Use mergeState for `SET_POSTS` reducer, add `SET_POST` reducer

--- src/ui/reducers/posts.js
+++ src/ui/reducers/posts.js
@@ -1,12 +1,10 @@
-import {reducerUtil} from './utils'
-
+import {reducerUtil, mergeState, mergeStateRenameKeys} from './utils'
 
 const reducers = {
 
-	SET_POSTS: (state, {posts}) => ({
-		...state,
-		...posts,
-	}),
+	SET_POSTS: mergeState,
+
+	SET_POST: mergeStateRenameKeys.bind(null, {post: 'currentPost'}),
 }
 
 export default reducerUtil.bind(null, reducers)
